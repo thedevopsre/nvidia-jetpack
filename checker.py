707199e5a917dae0a67a0333e2720e4ad684ac3b
Vahe <83751135+thedevopsre@users.noreply.github.com>
2024-08-22 11:53:44 +0400

    Changed script via library jtop and cleaned all modules list

--- checker.py
+++ checker.py
@@ -1,137 +1,111 @@
 import os
-import sys
-import subprocess
+import re
+import argparse
+from jtop.core.common import get_var
+from jtop.core.nvpmodel import nvpmodel_query
+from jtop.core.hardware import get_platform_variables
+from jtop.core.jetson_variables import get_jetson_variables
+from jtop.core.jetson_libraries import get_libraries, get_cuda, get_opencv
+from jtop.core.exceptions import JtopException
+from jtop.service import status_service
+from jtop.terminal_colors import bcolors
+# Version match
+VERSION_RE = re.compile(r""".*__version__ = ["'](.*?)['"]""", re.S)
+COPYRIGHT_RE = re.compile(r""".*__copyright__ = ["'](.*?)['"]""", re.S)
 
-MODULE_NAME_TABLE = {
-    'p3767-0005': 'NVIDIA Jetson Orin Nano (Developer kit)',
-    'p3767-0004': 'NVIDIA Jetson Orin Nano (4GB ram)',
-    'p3767-0003': 'NVIDIA Jetson Orin Nano (8GB ram)',
-    'p3767-0001': 'NVIDIA Jetson Orin NX (8GB ram)',
-    'p3767-0000': 'NVIDIA Jetson Orin NX (16GB ram)',
-    'p3701-0005': 'NVIDIA Jetson AGX Orin (64GB ram)',
-    'p3701-0004': 'NVIDIA Jetson AGX Orin (32GB ram)',
-    'p3701-0002': 'NVIDIA Jetson IGX Orin (Developer kit)',
-    'p3701-0000': 'NVIDIA Jetson AGX Orin',
-    'p3668-0003': 'NVIDIA Jetson Xavier NX (16GB ram)',
-    'p3668-0001': 'NVIDIA Jetson Xavier NX',
-    'p3668-0000': 'NVIDIA Jetson Xavier NX (Developer kit)',
-    'p2888-0008': 'NVIDIA Jetson AGX Xavier Industrial (32 GB ram)',
-    'p2888-0006': 'NVIDIA Jetson AGX Xavier (8 GB ram)',
-    'p2888-0005': 'NVIDIA Jetson AGX Xavier (64 GB ram)',
-    'p2888-0004': 'NVIDIA Jetson AGX Xavier (32 GB ram)',
-    'p2888-0003': 'NVIDIA Jetson AGX Xavier (32 GB ram)',
-    'p2888-0001': 'NVIDIA Jetson AGX Xavier (16 GB ram)',
-    'p3448-0003': 'NVIDIA Jetson Nano (2 GB ram)',
-    'p3448-0002': 'NVIDIA Jetson Nano module (16Gb eMMC)',
-    'p3448-0000': 'NVIDIA Jetson Nano (4 GB ram)',
-    'p3636-0001': 'NVIDIA Jetson TX2 NX',
-    'p3509-0000': 'NVIDIA Jetson TX2 NX',
-    'p3489-0888': 'NVIDIA Jetson TX2 (4 GB ram)',
-    'p3489-0000': 'NVIDIA Jetson TX2i',
-    'p3310-1000': 'NVIDIA Jetson TX2',
-    'p2180-1000': 'NVIDIA Jetson TX1',
-    'r375-0001': 'NVIDIA Jetson TK1',
-    'p3904-0000': 'NVIDIA Clara AGX',
-    # Other modules
-    'p2595-0000-A0': 'Nintendo Switch'
-}
 
-NVIDIA_JETPACK = {
-    # -------- JP6 --------
-    "36.2.0": "6.0 DP",
-    "36.0.0": "6.0 EA",
-    # -------- JP5 --------
-    "35.5.0": "5.1.3",
-    "35.4.1": "5.1.2",
-    "35.3.1": "5.1.1",
-    "35.3.0": "5.1.1 PRE",
-    "35.2.1": "5.1",
-    "35.1.0": "5.0.2 GA",
-    "34.1.1": "5.0.1 DP",
-    "34.1.0": "5.0 DP",
-    "34.0.1": "5.0 PRE-DP",
-    # -------- JP4 --------
-    "32.7.4": "4.6.4",
-    "32.7.3": "4.6.3",
-    "32.7.2": "4.6.2",
-    "32.7.1": "4.6.1",
-    "32.6.1": "4.6",
-    "32.5.2": "4.5.1",
-    "32.5.1": "4.5.1",
-    "32.5.0": "4.5",
-    "32.5": "4.5",
-    "32.4.4": "4.4.1",
-    "32.4.3": "4.4",
-    "32.4.2": "4.4 DP",
-    "32.3.1": "4.3",
-    "32.2.3": "4.2.3",
-    "32.2.1": "4.2.2",
-    "32.2.0": "4.2.1",
-    "32.2": "4.2.1",
-    "32.1.0": "4.2",
-    "32.1": "4.2",
-    "31.1.0": "4.1.1",
-    "31.1": "4.1.1",
-    "31.0.2": "4.1",
-    "31.0.1": "4.0",
-    # -------- Old JP --------
-    "28.4.0": "3.3.3",
-    "28.2.1": "3.3 | 3.2.1",
-    "28.2.0": "3.2",
-    "28.2": "3.2",
-    "28.1.0": "3.1",
-    "28.1": "3.1",
-    "27.1.0": "3.0",
-    "27.1": "3.0",
-    "24.2.1": "3.0 | 2.3.1",
-    "24.2.0": "2.3",
-    "24.2": "2.3",
-    "24.1.0": "2.2.1 | 2.2",
-    "24.1": "2.2.1 | 2.2",
-    "23.2.0": "2.1",
-    "23.2": "2.1",
-    "23.1.0": "2.0",
-    "23.1": "2.0",
-    "21.5.0": "2.3.1 | 2.3",
-    "21.5": "2.3.1 | 2.3",
-    "21.4.0": "2.2 | 2.1 | 2.0 | 1.2 DP",
-    "21.4": "2.2 | 2.1 | 2.0 | 1.2 DP",
-    "21.3.0": "1.1 DP",
-    "21.3": "1.1 DP",
-    "21.2.0": "1.0 DP",
-    "21.2": "1.0 DP",
-}
-
-# Run dpkg-query to get the version of nvidia-l4t-core
-def get_nvidia_l4t():
-    if sys.version_info >= (3, 7):  # Check if capture_output is supported
-        output = subprocess.run(['dpkg-query', '--showformat=\'${Version}\'', '--show', 'nvidia-l4t-core'], capture_output=True, text=True)
-        version = output.stdout.strip().split('-')[0].lstrip('\'')
+def main():
+    parser = argparse.ArgumentParser(
+        description='Show detailed information about this board. Machine, Jetpack, libraries and other',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-v', '--verbose', dest="verbose", help='Show all variables', action="store_true", default=False)
+    parser.add_argument('-s', '--serial', dest="serial", help='Show serial number', action="store_true", default=False)
+    # Copyrights
+    print("Software part of jetson-stats {version} - {copyright}".format(version=get_var(VERSION_RE), copyright=get_var(COPYRIGHT_RE)))
+    # Parse arguments
+    args = parser.parse_args()
+    # Read all Jetson Variables
+    jetson = get_jetson_variables()
+    # Extract and remove Serial Number
+    serial_number = jetson['Serial Number']
+    del jetson['Serial Number']
+    # Print headline
+    if jetson['Jetpack']:
+        print("Model: {model} - Jetpack {jetpack} [L4T {L4T}]".format(model=bcolors.bold(jetson['Model']),
+              jetpack=bcolors.bold(jetson['Jetpack']), L4T=bcolors.bold(jetson['L4T'])))
     else:
-        process = subprocess.Popen(['dpkg-query', '--showformat=\'${Version}\'', '--show', 'nvidia-l4t-core'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, _ = process.communicate()  # Get the stdout and ignore stderr
-        version = stdout.decode().strip().split('-')[0].lstrip('\'')
-    return version
+        print(bcolors.fail(bcolors.bold("Jetpack missing!")))
+        print(" - Model: {model}".format(model=bcolors.bold(jetson['Model'])))
+        print(" - L4T: {L4T}".format(L4T=bcolors.bold(jetson['L4T'])))
+    del jetson['Model']
+    del jetson['Jetpack']
+    del jetson['L4T']
+    # Print NVP model status
+    try:
+        nvpmodel_now = nvpmodel_query()
+        print("{service}[{number}]: {name}".format(
+            service=bcolors.ok(bcolors.bold("NV Power Mode")),
+            name=bcolors.bold(nvpmodel_now['name']),
+            number=bcolors.bold(nvpmodel_now['id'])))
+    except JtopException:
+        pass
+    # Print serial number
+    if serial_number:
+        if not args.serial:
+            serial_number = "[XXX Show with: jetson_release -s XXX]"
+        print("{sn_string} {serial_number}".format(sn_string=bcolors.ok(bcolors.bold("Serial Number:")), serial_number=serial_number))
+    # Print jetson hardware variables
+    if not args.verbose:
+        for name in ['699-level Part Number', 'SoC', 'Codename', 'BoardIDs', 'CUDA Arch BIN']:
+            if name in jetson:
+                del jetson[name]
+    print(bcolors.ok(bcolors.bold("Hardware:")))
+    for name, variable in jetson.items():
+        if not variable:
+            variable = bcolors.fail("Not available")
+            if name == 'Module' and os.getuid() != 0:
+                variable = bcolors.fail("Check with sudo")
+        print(" - {name}: {variable}".format(name=bcolors.bold(name), variable=variable))
+    # Print platform variables
+    plat = get_platform_variables()
+    if not args.verbose:
+        for name in ['Machine', 'System', 'Python']:
+            if name in plat:
+                del plat[name]
+    print(bcolors.ok(bcolors.bold("Platform:")))
+    for name, variable in plat.items():
+        if not variable:
+            variable = bcolors.fail("Not available")
+        print(" - {name}: {variable}".format(name=bcolors.bold(name), variable=variable))
+    # jtop status
+    print(bcolors.ok(bcolors.bold("jtop:")))
+    # Print status jetson-stats service
+    print(" - {name}: {value}".format(name=bcolors.bold("Version"), value=get_var(VERSION_RE)))
+    jtop_status = bcolors.ok("Active") if status_service() else bcolors.fail("Inactive")
+    print(" - {name}: {value}".format(name=bcolors.bold("Service"), value=jtop_status))
+    # Read CUDA status
+    print(bcolors.ok(bcolors.bold("Libraries:")))
+    cuda_version = get_cuda()
+    if cuda_version:
+        print(" - {name}: {value}".format(name=bcolors.bold('CUDA'), value=cuda_version))
+    else:
+        print(" - {name}: {value}".format(name=bcolors.bold('CUDA'), value=bcolors.fail("Not installed")))
+    # Read all libraries
+    os_variables = get_libraries()
+    for name, value in os_variables.items():
+        if not value:
+            value = bcolors.fail("Not installed")
+        print(" - {name}: {value}".format(name=bcolors.bold(name), value=value))
+    # Read OpenCV status
+    opencv_version, opencv_cuda = get_opencv()
+    if opencv_version:
+        opencv_cuda_string = bcolors.ok("YES") if opencv_cuda else bcolors.fail("NO")
+        opencv_string = "{name}: {value}".format(name=bcolors.bold('OpenCV'), value=opencv_version)
+        print(" - {opencv_string} - with CUDA: {opencv_cuda}".format(opencv_string=opencv_string, opencv_cuda=opencv_cuda_string))
+    else:
+        print(" - {name}: {value}".format(name=bcolors.bold('OpenCV'), value=bcolors.fail("Not installed")))
 
-nvidia_l4t_version = get_nvidia_l4t()
 
-if nvidia_l4t_version in NVIDIA_JETPACK:
-    result = NVIDIA_JETPACK[nvidia_l4t_version]
-else:
-    print("No corresponding NVIDIA JetPack version found.")
-
-# Read the output of /proc/device-tree/compatible
-with open("/proc/device-tree/compatible", "r") as file:
-    compatible_data = file.read().strip()
-
-# Check if the compatible_data matches any entry in the MODULE_NAME_TABLE
-matched_module = None
-for module_code, module_name in MODULE_NAME_TABLE.items():
-    if module_code in compatible_data:
-        matched_module = module_name
-        break
-
-if matched_module:
-    print(f"Device: {matched_module} Model: {result}")
-else:
-    print("No matching device found in the MODULE_NAME_TABLE.")
+if __name__ == "__main__":
+    main()
+# EOF
